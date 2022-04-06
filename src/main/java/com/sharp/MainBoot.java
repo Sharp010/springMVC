2ac4b93eb3468b61a644c4ab190b9213f17729e3
sharp <sharp@sharp.com>
2022-04-06 15:19:38 +0800

    save

---
+++ src/main/java/com/sharp/MainBoot.java
@@ -0,0 +1,11 @@
+package com.sharp;
+
+/**
+ * @author sharp
+ * @date 2022/4/6  15:16
+ */
+public class MainBoot {
+    public static void main(String[] args) {
+        System.out.println("hello my friend~");
+    }
+}
